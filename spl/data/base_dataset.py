Std and Var normalization options. Fixing h36m transformer issue.

spl/data/base_dataset.py
@@ -33,6 +33,7 @@ class Dataset(object):
         self.shuffle = shuffle
         self.normalize = kwargs.get("normalize", True)
         self.normalization_dim = kwargs.get("normalization_dim", "channel")  # "channel" or "all"
+        self.use_std_norm = kwargs.get("use_std_norm", False)  # Use variance or standard deviation of the data for normalization. This was a bug :) But we found that variance works much better for amass.
         
         if self.normalization_dim == "channel":
             self.normalization_func = self.normalize_zero_mean_unit_variance_channel
@@ -46,11 +47,14 @@ class Dataset(object):
         self.data_summary()
 
         self.mean_all = self.meta_data['mean_all']
-        # self.var_all = np.sqrt(self.meta_data['var_all'])
-        self.var_all = self.meta_data['var_all']
         self.mean_channel = self.meta_data['mean_channel']
-        # self.var_channel = np.sqrt(self.meta_data['var_channel'])
-        self.var_channel = self.meta_data['var_channel']
+        
+        if self.use_std_norm:
+            self.var_all = np.sqrt(self.meta_data['var_all'])
+            self.var_channel = np.sqrt(self.meta_data['var_channel'])
+        else:
+            self.var_all = self.meta_data['var_all']
+            self.var_channel = self.meta_data['var_channel']
 
         self.tf_data_transformations()
         self.tf_data_normalization()

spl/training.py
@@ -143,7 +143,9 @@ tf.app.flags.DEFINE_boolean("temp_abs_pos_encoding", False, "-")
 tf.app.flags.DEFINE_boolean("temp_rel_pos_encoding", False, "-")
 tf.app.flags.DEFINE_boolean("shared_templ_kv", False, "-")
 tf.app.flags.DEFINE_integer("max_relative_position", 50, "-")
+
 tf.app.flags.DEFINE_enum("normalization_dim", "channel", ["channel", "all"], "Channel-wise or global normalization.")
+tf.app.flags.DEFINE_boolean("use_std_norm", False, "-")
 
 args = tf.app.flags.FLAGS
 
@@ -245,7 +247,8 @@ def create_model(session):
                                            window_type=C.DATA_WINDOW_RANDOM,
                                            num_parallel_calls=4,
                                            normalize=not config["no_normalization"],
-                                           normalization_dim=config.get("normalization_dim", "channel"))
+                                           normalization_dim=config.get("normalization_dim", "channel"),
+                                           use_std_norm=config.get("use_std_norm", False))
         train_pl = train_data.get_tf_samples()
     
     with tf.name_scope("validation_data"):
@@ -259,7 +262,8 @@ def create_model(session):
                                            window_type=C.DATA_WINDOW_CENTER,
                                            num_parallel_calls=4,
                                            normalize=not config["no_normalization"],
-                                           normalization_dim=config.get("normalization_dim", "channel"))
+                                           normalization_dim=config.get("normalization_dim", "channel"),
+                                           use_std_norm=config.get("use_std_norm", False))
         valid_pl = valid_data.get_tf_samples()
     
     with tf.name_scope("test_data"):
@@ -273,7 +277,8 @@ def create_model(session):
                                           num_parallel_calls=4,
                                           normalize=not config["no_normalization"],
                                           normalization_dim=config.get("normalization_dim", "channel"),
-                                          beginning_index=beginning_index)
+                                          beginning_index=beginning_index,
+                                          use_std_norm=config.get("use_std_norm", False))
         test_pl = test_data.get_tf_samples()
 
     # Models.
@@ -324,7 +329,8 @@ def create_model(session):
                                                   # extract_random_windows=False,
                                                   num_parallel_calls=4,
                                                   normalize=not config["no_normalization"],
-                                                  normalization_dim=config.get("normalization_dim", "channel"))
+                                                  normalization_dim=config.get("normalization_dim", "channel"),
+                                                  use_std_norm=config.get("use_std_norm", False))
             srnn_pl = srnn_data.get_tf_samples()
 
         with tf.name_scope("SRNN"):